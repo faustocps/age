de3396ccc073b59352429fb51b99a348170fc8c9
Fausto Candido <fausto.candido@evtit.com>
2025-04-17 16:01:10 -0300

    refactoring files

---
+++ js/main.js
@@ -0,0 +1,39 @@
+function generateRandomColor() {
+  const cores = [
+    "#196F3D",
+    "#2E4053",
+    "#5B2C6F",
+    "#922B21",
+    "#1A5276",
+    "#121212",
+    "#b9770e",
+    "#616a6b",
+    "#008080",
+  ];
+  return cores[Math.floor(Math.random() * cores.length)];
+}
+
+document.body.style.backgroundColor = generateRandomColor();
+
+function age(dateOfBirth) {
+  const today = new Date();
+  const birth = new Date(dateOfBirth);
+  const diffMs = today - birth;
+  const msPerYear = 1000 * 60 * 60 * 24 * 365.2425;
+  return diffMs / msPerYear;
+}
+
+function formatAge(age) {
+  return `Fausto is ${age.toFixed(10)} years old!`;
+}
+
+const dateOfBirth = "1991-10-20";
+const fieldAge = document.getElementById("age");
+
+function updateAge() {
+  const value = age(dateOfBirth);
+  fieldAge.textContent = formatAge(value);
+}
+
+updateAge();
+setInterval(updateAge, 100);
